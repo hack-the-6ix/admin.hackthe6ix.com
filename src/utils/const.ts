8a9bbd2807b0c55bc490e26c679f2d7a8852687d
Aurora Shi <aurorashi16@gmail.com>
2025-01-19 21:16:11 -0500

    Created Review Frontend

---
+++ src/utils/const.ts
@@ -0,0 +1,22 @@
+export const maxPerCategory = {
+  creativeResponseEssay: 4,
+  whyHT6Essay: 4,
+  project: 4,
+  portfolioLink: 2,
+};
+
+export const categoryNames = {
+  creativeResponseEssay: 'Creative Response Essay',
+  whyHT6Essay: 'Why Hack the 6ix Essay',
+  project: 'Tell us About a Project',
+  portfolioLink: 'Portfolio Link',
+};
+
+export const APINames = {
+  creativeResponseEssay: 'creativeResponse',
+  whyHT6Essay: 'whyHT6',
+  project: 'project',
+  portfolioLink: 'portfolio',
+};
+
+export const ratingScale = ['🤬', '😐', '🙂', '😁', '😍'];
