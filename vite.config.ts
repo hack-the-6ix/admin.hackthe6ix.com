f6e4c57ad5c2edd220cf623053704f4bdcb3f500
jeffrey-zang <jeffrey.zang.07@gmail.com>
2025-04-27 23:26:11 -0400

    add index.html to root

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,15 @@
 import { reactRouter } from '@react-router/dev/vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [reactRouter(), tsconfigPaths()],
+  plugins: [reactRouter(), tsconfigPaths(), react()],
   server: {
     port: 3000,
+  },
+  build: {
+    outDir: 'build/client',
+    emptyOutDir: true,
   },
 });
